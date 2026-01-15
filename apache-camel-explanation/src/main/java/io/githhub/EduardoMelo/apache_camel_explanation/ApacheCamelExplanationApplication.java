5b4f300ad21138a5bc3eaff197bfefa1136045b0
Eduardo Melo <eduardo.moliveira.dev@hotmail.com>
2026-01-15 18:51:00 -0300

    chore: início do projeto de estudos com o apache camel

---
+++ apache-camel-explanation/src/main/java/io/githhub/EduardoMelo/apache_camel_explanation/ApacheCamelExplanationApplication.java
@@ -0,0 +1,13 @@
+package io.githhub.EduardoMelo.apache_camel_explanation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApacheCamelExplanationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApacheCamelExplanationApplication.class, args);
+	}
+
+}
---
+++ apache-camel-explanation/src/test/java/io/githhub/EduardoMelo/apache_camel_explanation/ApacheCamelExplanationApplicationTests.java
@@ -0,0 +1,13 @@
+package io.githhub.EduardoMelo.apache_camel_explanation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApacheCamelExplanationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
