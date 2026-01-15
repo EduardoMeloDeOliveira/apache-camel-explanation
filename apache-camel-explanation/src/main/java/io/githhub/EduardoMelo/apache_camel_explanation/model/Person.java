c34eda3b4658aadb16fe68ba9ac591ff8c791cad
Eduardo Melo <eduardo.moliveira.dev@hotmail.com>
2026-01-15 18:55:08 -0300

    feat: incia a criação da camada de model

---
+++ apache-camel-explanation/src/main/java/io/githhub/EduardoMelo/apache_camel_explanation/model/Person.java
@@ -0,0 +1,26 @@
+package io.githhub.EduardoMelo.apache_camel_explanation.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import lombok.*;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Person {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long Id;
+
+    @NotBlank
+    private String name;
+
+    @Min(18)
+    private int age;
+}
